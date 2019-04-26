31373070471ddee54ec6bd9b1ba0a1b20aa4dc8f
Rahmatsyah <rahmatsyah1317@gmail.com>
2019-04-27 05:35:33 +0700

    List pesanan dan Profile

--- app/src/main/java/com/hackjam/sevenshop/fragment/ProfileFragment.java
+++ app/src/main/java/com/hackjam/sevenshop/fragment/ProfileFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hackjam.sevenshop.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
+    private View rootView;
+
+    private CircleImageView iv_foto;
+    private TextView nama, editProfil, editPassword, syaratKetentuan, keluar;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,9 +30,30 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        iv_foto = rootView.findViewById(R.id.iv_profile_foto);
+        nama = rootView.findViewById(R.id.tv_profile_nama);
+        editProfil = rootView.findViewById(R.id.tv_profile_edit);
+        editPassword = rootView.findViewById(R.id.tv_profile_ganti_pass);
+        syaratKetentuan = rootView.findViewById(R.id.tv_profile_syarat_dan_ketentuan);
+        keluar = rootView.findViewById(R.id.tv_profile_keluar);
+
+        return rootView;
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_profile_edit :
+                break;
+            case R.id.tv_profile_ganti_pass :
+                break;
+            case R.id.tv_profile_syarat_dan_ketentuan :
+                break;
+            case R.id.tv_profile_keluar :
+                break;
+        }
+    }
 }
