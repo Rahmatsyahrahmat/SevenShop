d8a816ee9df69b87f92296479d5e60612f58861a
Rahmatsyah <rahmatsyah1317@gmail.com>
2019-04-28 16:05:38 +0700

    Merge branch 'master' of https://github.com/Rahmatsyahrahmat/SevenShop
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/hackjam/sevenshop/fragment/HomeFragment.java
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/com/hackjam/sevenshop/adapter/ProdukBaruAdapter.java
+++ app/src/main/java/com/hackjam/sevenshop/adapter/ProdukBaruAdapter.java
@@ -1,29 +1,43 @@
 package com.hackjam.sevenshop.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.hackjam.sevenshop.R;
+import com.hackjam.sevenshop.activity.DetailProduk;
 import com.hackjam.sevenshop.model.itemProduk;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class ProdukBaruAdapter extends RecyclerView.Adapter<ProdukBaruAdapter.ViewHolder> {
+public class ProdukBaruAdapter extends RecyclerView.Adapter<ProdukBaruAdapter.ViewHolder> implements Filterable {
 
-    private ArrayList<itemProduk> produks;
+    private ArrayList<itemProduk> produks, dataSet;
     private Context context;
 
     public ProdukBaruAdapter(Context context, ArrayList<itemProduk> produks){
         this.context = context;
         this.produks = produks;
+        dataSet = produks;
     }
 
     @NonNull
@@ -34,11 +48,33 @@ public class ProdukBaruAdapter extends RecyclerView.Adapter<ProdukBaruAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        itemProduk produk = produks.get(i);
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
+        final itemProduk produk = produks.get(i);
         viewHolder.judul.setText(produk.getNama());
         viewHolder.harga.setText(produk.getHargaPotongan());
         viewHolder.progres.setText(produk.getJumlahJoin()+"/"+produk.getMaxJoin());
+        viewHolder.progressBar.setProgress(produk.getJumlahJoin());
+        viewHolder.progressBar.setMax(50);
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Produk").child(produk.getId()+"_1.jpg");
+        try {
+            final File localFile = File.createTempFile("Images", "jpg");
+            storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    if(localFile!=null)viewHolder.foto.setImageBitmap(BitmapFactory.decodeFile(localFile.getAbsolutePath()));
+                }
+            });
+        } catch (IOException e) {
+
+        }
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailProduk.class);
+                intent.putExtra("Produk",produk);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -46,9 +82,45 @@ public class ProdukBaruAdapter extends RecyclerView.Adapter<ProdukBaruAdapter.Vi
         return produks.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    produks = dataSet;
+                } else {
+                    ArrayList<itemProduk> filteredList = new ArrayList<>();
+                    for (itemProduk row : dataSet) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getNama().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    produks = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = dataSet;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                dataSet = (ArrayList<itemProduk>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView judul, harga, progres;
-        private RoundCornerProgressBar progressBar;
+       // private RoundCornerProgressBar progressBar;
+        private ProgressBar progressBar;
         private ImageView foto;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
