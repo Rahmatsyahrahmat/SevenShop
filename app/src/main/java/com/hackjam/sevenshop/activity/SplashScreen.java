3992295216914ec1aba848df843ef402f36e47df
atifa <atifafiorenza24@gmail.com>
2019-05-18 01:06:04 +0700

    Merge branch 'master' of https://github.com/Rahmatsyahrahmat/SevenShop
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/hackjam/sevenshop/fragment/HomeFragment.java
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_register.xml
    #	app/src/main/res/layout/item_produk_baru.xml

--- app/src/main/java/com/hackjam/sevenshop/activity/SplashScreen.java
+++ app/src/main/java/com/hackjam/sevenshop/activity/SplashScreen.java
@@ -4,15 +4,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.hackjam.sevenshop.R;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ImageView logo1,logo2,logo3;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.spalshscreen);
+        setContentView(R.layout.activity_splash);
 
-        startActivity(new Intent(SplashScreen.this,LoginActivity.class));
+        logo1 = findViewById(R.id.iv_logo1_ss);
+        logo2 = findViewById(R.id.iv_logo2_ss);
+        logo3 = findViewById(R.id.iv_logo3_ss);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.splashanimation);
+        logo1.startAnimation(animation);
+        logo2.startAnimation(animation);
+        logo3.startAnimation(animation);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(4000);
+                    Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        };
+        thread.run();
     }
 }
